96d99f880572bb97eb7ad4f6ea06e3e2d6394d35
whjeonlocal <whjeonlocal@gmail.com>
2024-02-09 22:04:10 +0900

    [전우형] 프로젝트 정리(+엑셀파일 추가)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace AxcelConverter
+﻿using Aspose.Cells;
+
+namespace Whjeon
 {
     public class Program
     {
+
+        /// <summary>        
+        /// 1.Design 폴더내 엑셀파일들을 읽어온다
+        /// 2.Json으로 변환한다.
+        /// 3.이미 있는경우, 비교하여 다르다면 덮어쓴다.
+        /// </summary>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine(Directory.Exists("Hello"));            
         }
     }
 }
