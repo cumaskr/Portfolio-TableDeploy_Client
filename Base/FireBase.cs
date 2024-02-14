e8e8cce477873f510fea419c0ccc098668f89dd5
whjeonlocal <whjeonlocal@gmail.com>
2024-02-14 18:07:05 +0900

    [전우형] FireBase 클래스 로직 정리

--- Base/FireBase.cs
+++ Base/FireBase.cs
@@ -1,20 +1,20 @@
-﻿using Google.Cloud.Firestore;
+﻿using Amazon.S3;
+using dotenv.net;
+using Google.Cloud.Firestore;
 
 namespace LocalTableBuilder
 {
-
     /// <summary>
     /// FireBase(저장소) 클래스[whjeon 24.02.12]
     /// 사용용도 : 테이블 버전별 Json 저장, 서버에 파일 저장 시 알림 날림
     /// 사용방법
-    /// 1)path 경로에 저장소에서 받은 프로젝트 키(Json)파일 추가
-    /// 2)해당파일 빌드 시, 포함되도록 변경
-    /// 3)해당 클래스 사용    
+    /// 1)FireBase.json 파일에 인증값 수동으로 입력(GitHub에는 빈파일 올라감)
+    /// 2)해당 클래스 사용
     /// </summary>
-    public class FireBase
+    public class FireBase : IDisposable, IExternalStore
     {
         //저장소 접근하기위한 Json파일명(해당 파일은 깃에 올라가지 않음, 특정 컴퓨터 내부에서 관리)
-        private string AdminSdkJson = "tablebuild-e6f20-firebase-adminsdk-xx0ap-d795853497.json";
+        private string AdminSdkJson = "FireBase.json";
         private string ProjectId = "tablebuild-e6f20";
         private string Server = "Local";
         private string DocumentName = "Table";
@@ -25,22 +25,68 @@ namespace LocalTableBuilder
         //생성자(초기화)
         public FireBase()
         {
+            Console.WriteLine("[FireBase] Start Setting");
+
             Db = null;
-            string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\{AdminSdkJson}";
-            if (File.Exists(path))
+            string path = $"./{AdminSdkJson}";
+
+            try
             {
                 Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
-                Db = FirestoreDb.Create(ProjectId);                
+                Db = FirestoreDb.Create(ProjectId);
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Db = null;
+                return;
+            }
+
+            Console.WriteLine("[FireBase] Ok");
         }
+
         //테이블 최신 버전 업로드
         public void UpdateTableVersions(Dictionary<string, int> recentVersions) 
         {
-            if (null != Db) 
+            if (null != Db)
             {
                 var collection = Db.Collection(Server).Document(DocumentName);
                 collection.SetAsync(recentVersions).GetAwaiter().GetResult();
             }
+        }
+
+        //리소스 정리
+        public void Dispose()
+        {
+            Db = null;
+        }
+
+        //연결 확인
+        public bool IsOk()
+        {
+            Console.WriteLine("[FireBase] Check Connect");
+
+            if (null == Db)
+            {
+                return false;
+            }
+            else
+            {
+                try
+                {
+                    var collection = Db.Collection(Server).Document("Test");
+                    collection.SetAsync(new Dictionary<string, int>()).GetAwaiter().GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+
+            Console.WriteLine("[FireBase] Ok");
+
+            return true;
         }
     }
 }
