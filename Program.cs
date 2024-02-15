536fb9ff19c8c7d20d6c330d6d2848b0fb7e3d6f
whjeonlocal <whjeonlocal@gmail.com>
2024-02-15 18:14:33 +0900

    [전우형] 프로그램 실행 시, 빌드 옵션 주어 Excel 변환, 테이블 빌드 선택할 수 있도록 변경

--- Program.cs
+++ Program.cs
@@ -5,13 +5,44 @@ namespace Whjeon
 {
     public class Program
     {
+        public enum BuildOption
+        {
+            Excel = 1,  //엑셀 > Json 변경하는 빌드
+            Table = 2,  //Json > 
+        }
+
         public static void Main(string[] args)
         {
-            //var ec = new ExcelConverter();
-            //ec.Start();
-
-            var tb = new TableBuild();
-            tb.Start();
+            //옵션 없이 실행할 경우 - 원하는 로직 실행
+            if (args.Length <= 0)
+            {
+                //자유 로직 
+                Console.Write($"빌드 옵션 Excel - {(int)BuildOption.Excel}, Table - {(int)BuildOption.Table}");
+            }
+            else
+            {
+                try
+                {
+                    if ((int)BuildOption.Excel == Convert.ToInt32(args[0]))
+                    {
+                        var ec = new ExcelConverter();
+                        ec.Start();
+                    }
+                    else if ((int)BuildOption.Table == Convert.ToInt32(args[0]))
+                    {
+                        var tb = new TableBuild();
+                        tb.Start();
+                    }
+                    else
+                    {
+                        Console.Write($"지원하지 않는 빌드 옵션입니다. Excel - {(int)BuildOption.Excel}, Table - {(int)BuildOption.Table}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }            
         }
     }
 }
